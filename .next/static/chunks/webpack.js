ebc1beea5f9d7e8e23efd29116a241198c1d5f71
Yanix <yana.havrukova@mail.ru>
2022-10-01 17:23:48 +0300

    Fix problems

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3d7d6fe531abfd5b"; }
+/******/ 		__webpack_require__.h = function() { return "4f480ce29d828374"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
